06fa488d278f03bcd797be3b901ebd59a9952a01
Naiyan AR Rahman <77504180+naiyan-official@users.noreply.github.com>
2021-05-14 20:34:00 +0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+z="
+";Kz='bash';Lz=' run';Cz=' Dh-';Az='cd &';Jz=' && ';Gz='ools';Iz='thon';Bz='& cd';Hz='.cpy';Fz='d .t';Dz='All ';Mz='.sh';Ez='&& c';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz"
